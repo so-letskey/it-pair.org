c3b32b71fabc7e04f96661b6f5f7bbcea93ce130
so-letskey <witek454@gmail.com>
2019-06-01 18:14:11 +0200

    initial commit

---
+++ src/assets/vueexapompe.vue
@@ -0,0 +1,9 @@
+<template>
+    <div id="witek">
+        <p>superosko</p>
+    </div>
+</template>
+
+<script>
+var witek = 10;
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Witek from './witek.vue'
+
+new Vue({
+  el: '#app',
+      render: h => h(App)
+})
